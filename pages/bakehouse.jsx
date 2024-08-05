63db244eae97bb5faaf485067fd4339254cb15bc
Ahnaaf Khan <ahnaafk@gmail.com>
2024-08-05 18:20:43 -0400

    Fix image

--- pages/bakehouse.jsx
+++ pages/bakehouse.jsx
@@ -29,67 +29,59 @@ import { SiGmail } from 'react-icons/si'
 const Page = () => {
   return (
     <Layout>
-      <Container maxW={{base:'100%', md: "60%"}}>
-          <Box
-            flexShrink={8}
-            mt={{ base: 5, md: 4 }}
-            align="center"
-          >
-            <Image
-              borderColor="whiteAlpha.800"
-              borderWidth={0}
-              borderStyle="solid"
-              maxWidth={{md: "50%", base:"70%"}}
-              display="inline-block"
-              src="/images/bakehouse/bakehouse-light.png"
-              alt="bakehouse logo"
-            />
-          </Box>
+      <Container maxW={{ base: '100%', md: '70%' }}>
+        <Box flexShrink={8} mt={{ base: 5, md: 4 }} align="center">
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={0}
+            borderStyle="solid"
+            maxWidth={{ md: '50%', base: '70%' }}
+            display="inline-block"
+            src="/images/bakehouse/bakehouse-light.png"
+            alt="bakehouse logo"
+          />
+        </Box>
         <Section delay={0.1}>
-          <Box display={{ md: 'flex' }} align="center">
-            <Box
-              flexShrink={8}
-              mr={{ md: 6 }}
-              align="left"
-            >
+          <Box display={{ md: 'flex' }} align="center" flexShrink={8}>
+            <Box mr={{ md: 6 }} align="left">
               <Heading as="h3" variant="section-title">
                 Welcome to the House!
               </Heading>
               <Text text-align="centered">
-                We&apos;re a pop-up artisanal bakeshop in Bowmanville & Kingston.
-                Currently, we exclusively sell NY-style cheesecakes, check out
-                our menu.
+                We&apos;re a pop-up artisanal bakeshop in Bowmanville &
+                Kingston. Currently, we exclusively sell NY-style cheesecakes,
+                check out our menu.
               </Text>
               <br />
               <Text>
-              I started to bake as a hobby when I didn&apos;t want to do my engineering homework and 
-              since then my skills as a baker have developed quite a bit. 
-              A lot of my close friends and family have told me that my cheesecake is really good, 
-              and that I should try selling it.
+                I started to bake as a hobby when I didn&apos;t want to do my
+                engineering homework and since then my skills as a baker have
+                developed quite a bit. A lot of my close friends and family have
+                told me that my cheesecake is really good, and that I should try
+                selling it.
               </Text>
               <br />
-              <Text>
-              That&apos;s what I&apos;m doing here. Hope you enjoy!
-              </Text>
-              <Link href="https://www.instagram.com/bakehouse.ahnaafk/" isExternal>
-              <Button
-                rightIcon={<ChevronRightIcon />}
-                colorScheme="blue"
-                my={'10%'}
-                href="instagram.com"
+              <Text>That&apos;s what I&apos;m doing here. Hope you enjoy!</Text>
+              <Link
+                href="https://www.instagram.com/bakehouse.ahnaafk/"
+                isExternal
               >
-                Order now!
-              </Button>
-            </Link>
+                <Button
+                  rightIcon={<ChevronRightIcon />}
+                  colorScheme="blue"
+                  my={'10%'}
+                  href="instagram.com"
+                >
+                  Order now!
+                </Button>
+              </Link>
             </Box>
             <Image
-              borderColor="whiteAlpha.800"
-              borderWidth={0}
-              borderStyle="solid"
-              maxWidth="60%"
+              maxWidth="50%"
               display="inline-block"
               src="/images/bakehouse/bakehouse-flyer.png"
-              alt="bakehouse logo"
+              alt="bakehouse flyer"
+              objectFit='contain'
             />
           </Box>
         </Section>
